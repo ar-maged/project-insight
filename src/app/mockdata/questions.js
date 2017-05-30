6fb13ededac69d36b7f7f927f31c4ece1421f863
A.R. Maged <abdulrahman.maged95@gmail.com>
2017-05-30 16:45:59 +0200

    :pencil: Add more dummy questions

--- src/app/mockdata/questions.js
+++ src/app/mockdata/questions.js
@@ -1,7 +1,29 @@
 export default [
   {
     _id: '58d56a11e619fe7424bdd82c',
-    title: 'Are you a lecturer?',
+    title: 'How many hours do you study per day?',
+    choices: [
+      {
+        index: 0,
+        title: '< 1'
+      },
+      {
+        index: 1,
+        title: '1 - 3'
+      },
+      {
+        index: 2,
+        title: '3 - 5'
+      },
+      {
+        index: 3,
+        title: '> 5'
+      }
+    ]
+  },
+  {
+    _id: '58d56a11e619fe7424bdd83f',
+    title: 'Do you think having a bachelor degree is essential for landing a respectable job in the corporate world?',
     choices: [
       {
         index: 0,
